fe239a6e1b6c7727f15f97546d57b8fc191ee9c6
Dominykas <dominykaseicinas>
2023-09-01 21:27:08 +0300

    adding the files that were missed in the previous commits

---
+++ src/lib/components/CreateQuizForm.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Button from './Button.svelte';
+
+	let name = '';
+	let description = '';
+
+	const submitQuiz = () => {
+		console.log({
+			message: 'sending request',
+			data: {
+				name: name,
+				description: description
+			}
+		});
+	};
+</script>
+
+<div class="card card-normal bg-secondary mt-20 rounded-xl">
+	<div class="card-body flex items-center flex-col">
+		<input
+			type="text"
+			bind:value={name}
+			placeholder="Give your quiz a title!"
+			class="mb-2 p-2 h-10 w-80 rounded-lg"
+		/>
+		<textarea
+			bind:value={description}
+			placeholder="... and a description :)"
+			class="p-2 h-32 w-80 rounded-lg"
+		/>
+		<button on:click={submitQuiz} class="btn-active mt-1 p-2 rounded-lg"> create!! </button>
+	</div>
+</div>
