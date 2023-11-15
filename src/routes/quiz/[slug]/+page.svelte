4059a31dda43352cf011236e35dacf5d64002421
Dominykas <dominykas.eicinas@gmail.com>
2023-11-15 22:16:25 +0200

    fixed the app crash on 404

--- src/routes/quiz/[slug]/+page.svelte
+++ src/routes/quiz/[slug]/+page.svelte
@@ -1,24 +1,31 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import { PUBLIC_API_URL } from '$env/static/public';
 	import QuizOverviewComponent from '$lib/components/QuizOverviewComponent.svelte';
 	import type { Quiz } from '$lib/types/Quiz';
 	import axios from 'axios';
 
-	let slug: number;
-	let quiz: Quiz;
+	let slug: number|undefined = undefined;
+	let quiz: Quiz|undefined = undefined;
 
 	$: {
 		slug = parseInt($page.params.slug);
 	}
 
-	$: {
+    $: {
 		axios.get(PUBLIC_API_URL + `/quiz/${slug}`).then((res) => {
 			if (res.status == 200) {
 				quiz = res.data;
 			}
-		});
-	}
+		}).catch((e) => {
+            console.log(e);
+            
+            if (browser) {
+                window.location.href = '/404';
+            }
+        })
+    }
 </script>
 
 {#if quiz == undefined}
