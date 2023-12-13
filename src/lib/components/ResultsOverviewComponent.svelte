e4e50296c76b1e058c963384ea87839cb6da94dc
Dominykas <dominykas.eicinas@gmail.com>
2023-12-13 19:42:12 +0200

    added answer frequencies section

--- src/lib/components/ResultsOverviewComponent.svelte
+++ src/lib/components/ResultsOverviewComponent.svelte
@@ -3,6 +3,7 @@
 	import type { Quiz } from "$lib/types/Quiz";
 	import type { ResultsOverview } from "$lib/types/results/ResultsOverview";
 	import { onMount } from "svelte";
+	import type { FrequenciesAnswer } from "$lib/types/results/FrequenciesAnswer";
 
     export let quiz: Quiz;
     export let results: ResultsOverview;
@@ -42,6 +43,12 @@
             },
         });
     });
+
+    const calculatePercentage = (all: FrequenciesAnswer[], answer: FrequenciesAnswer): string => {
+        let sum: number = 0;
+        all.forEach((a) => sum += a.count);
+        return Math.round((answer.count / sum) * 100) + "%";
+    }
 </script>
 
 <div class="h-full w-2/3">
@@ -57,5 +64,26 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="my-8 w-full border border-primary border-solid p-10">
+        <h1 class="text-3xl font-bold mb-4">Answer frequencies</h1>
+        {#each results.frequencies as question}
+            <div class="w-full h-wrap m-1 collapse collapse-arrow bg-primary rounded-none border-2 border-solid border-primary text-white">
+                <input type="checkbox" />
+                <div class="collapse-title w-wrap flex justify-center flex-col">
+                    <div class="flex w-full flex-row items-start">
+                        <span class="text-white font-bold text-lg">{question.questionText}</span>
+                    </div>
+                </div>
+                <div class="collapse-content bg-white w-full flex flex-col">
+                    {#each question.answers as answer}
+                        <div class="w-full flex flex-row justify-between items-center p-2 {question.answers.indexOf(answer) < question.answers.length - 1 ? 'border-b-primary border-solid border border-x-white' : ''}">
+                            <span class="text-xl text-black">{answer.answerText}</span>
+                            <span class="text-xl text-white bg-primary rounded-full px-2 py-1">{calculatePercentage(question.answers, answer)}</span>
+                        </div>
+                    {/each}
+                </div>
+            </div>
+        {/each}
     </div>
 </div>
